d81a4cf0a387db98b8e0d351592b8b1696fbc545
Dominik Dobrowolski <76428521+ddominet@users.noreply.github.com>
2022-12-04 14:00:30 +0100

    test
    -testing workflow code-ql

---
+++ app.py
@@ -0,0 +1,76 @@
+#test file
+
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+
+app = Flask(__name__)
+
+recipes = [
+    {"id": 1, "name": "DUPA1", "description": "DUPA1"},
+    {
+        "id": 2,
+        "name": "DUPA2",
+        "description": "DUPA2",
+    },
+]
+
+
+@app.route("/recipes", methods=["GET"])
+def get_recipes():
+    return jsonify({"data": recipes})
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["GET"])
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] is recipe_id), None)
+
+    if recipe:
+        return jsonify(recipe)
+
+    return jsonify({"message": "file not found"}), HTTPStatus.NOT_FOUND
+
+
+@app.route("/recipes", methods=["POST"])
+def create_recipe():
+    data = request.get_json()
+
+    name = data.get("name")
+    description = data.get("description")
+
+    recipe = {"id": len(recipes) + 1, "name": name, "description": description}
+
+    recipes.append(recipe)
+
+    return jsonify(recipe), HTTPStatus.CREATED
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["PUT"])
+def update_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] is recipe_id), None)
+
+    if not recipe:
+        return jsonify({"message": "file not found"}), HTTPStatus.NOT_FOUND
+
+    data = request.get_json()
+
+    recipe.update({"name": data.get("name"), "description": data.get("description")})
+
+    return jsonify(recipe)
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["DELETE"])
+def delete_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe["id"] is recipe_id), None)
+
+    if not recipe:
+        return (
+            jsonify({"message": "Cannot delete, resource does not exist"}),
+            HTTPStatus.NOT_FOUND,
+        )
+
+    recipes.remove(recipe)
+    return jsonify({"message": "deleted!"}), HTTPStatus.ACCEPTED
+
+
+if __name__ == "__main__":
+    app.run()
